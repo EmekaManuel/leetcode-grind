6c10dff9c27dedb695b7da02290dbf5a88fa0bea
Onyewuchi Emeka <marrnuel123@.com>
2023-04-28 00:18:32 +0100

    daily commit

--- palindrome.js
+++ palindrome.js
@@ -2,20 +2,20 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function (x) {
-  //TIME-COMPLEXITY: O(n);
-  //MEMORY: O(1);
-  const str = x.toString();
-  let leftIndex = 0;
-  let rightIndex = str.length - 1;
-
-  while (leftIndex < rightIndex) {
-    if (str.charAt(leftIndex) !== str.charAt(rightIndex)) {
-      return false;
+var isPalindrome = function(x) {
+    if (x === 0) {
+        return true;
+    } else {
+        const reverse = x.toString().split("").reverse().join("");
+        // split convert array
+        // reverse array
+        // join return string
+        if (x == reverse) {
+            return true;
+        }
+        if (x != reverse) {
+            return false;
+        }
     }
-    leftIndex++;
-    rightIndex--;
-  }
-
-  return true;
 };
+
