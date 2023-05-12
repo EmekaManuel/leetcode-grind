10a1d0d1aaec10580677ea4dfbb3a316831bb448
Onyewuchi Emeka <marrnuel123@.com>
2023-05-12 15:09:00 +0100

    leetcode grind

---
+++ removeDuplicates.js
@@ -0,0 +1,47 @@
+// Solution 1 : Using Array splice method
+const removeDuplicates = (nums) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == nums[i + 1]) {
+      nums.splice(i + 1, 1);
+      i--;
+    }
+  }
+};
+
+// Solution 2 : For loop with continue
+const removeDuplicates = (nums) => {
+  if (nums.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] === nums[i]) continue; // If the elements are equal then continue else copy the unique element by then to "i+1" position
+    nums[++i] = nums[j];
+  }
+  return i + 1;
+};
+
+// Solution 3 : For loop
+const removeDuplicates = (nums) => {
+  if (nums.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      //  If the elements are not equal then copy the unique element by then to "i+1" position
+      nums[++i] = nums[j];
+    }
+  }
+  return i + 1;
+};
+
+// Solution 4 :  While loop
+const removeDuplicates = (nums) => {
+  if (nums.length === 0) return 0;
+  let i = 0;
+  let j = 1;
+  while (j < nums.length) {
+    if (nums[j] !== nums[i]) {
+      nums[++i] = nums[j];
+    }
+    j++;
+  }
+  return i + 1;
+};
