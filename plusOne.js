287ff4a1576f9e2c3295a95bb088774e6966cb6f
Onyewuchi Emeka <marrnuel123@.com>
2023-05-14 23:44:46 +0100

    added slight modification

--- plusOne.js
+++ plusOne.js
@@ -2,12 +2,11 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
-    let string = '';
-    for(digit in digits){
-      string += digits[digit];
-    }
-    digits = BigInt(string) + BigInt(1);
-    return Array.from(String(digits), num => Number(num));
-  };
-  
+const plusOne = (digits) => {
+  let string = "";
+  for (let digit in digits) {
+    string += digits[digit];
+  }
+  digits = BigInt(string) + BigInt(1);
+  return Array.from(String(digits), (num) => Number(num));
+};
